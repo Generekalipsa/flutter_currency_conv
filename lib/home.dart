18b472d3dd0e917a9a181636fb2423651f244b0a
fifi <djgamez1222@gmail.com>
2025-01-18 12:30:06 +0100

    Adding HomeScreenState and beggining to build application Scaffold

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+  }
+
+class _HomeScreenState extends State<HomeScreen> {
+  String fromCurrency = 'USD';
+  String forCurrency = 'PLN';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
