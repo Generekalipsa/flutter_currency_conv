0dbc09912d3a58143f8bc4bb5c81251cb7e91f8a
fifi <djgamez1222@gmail.com>
2025-01-18 12:47:12 +0100

    Fixed error by changing StatelessWidget to StatefulWidget

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
@@ -10,8 +10,21 @@ class HomeScreen extends StatelessWidget {
 class _HomeScreenState extends State<HomeScreen> {
   String fromCurrency = 'USD';
   String forCurrency = 'PLN';
+  double rate = 0.0;
+  double total = 0.0;
+  TextEditingController amountController = TextEditingController();
+  List<String> currencies = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFF2c3e50),
+    );
   }
 }
